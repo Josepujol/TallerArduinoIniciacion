c43183ffd33ac67422250d73feeab9146c29c56c
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2019-04-01 20:31:31 +0200

    Create PulsadorZumbador.ino

---
+++ EntradasDigitales/PulsadorZumbador/PulsadorZumbador/PulsadorZumbador.ino
@@ -0,0 +1,40 @@
+/*
+ Pulsador Zumbador
+ Sistema para controlar un zumbador con un pulsador
+ 
+ Hardware: 
+ Pulsador pin 2
+ Zumbador pin 12
+ 
+ Jose Pujol
+ Septiembre 2017
+ */
+
+// constantes que no cambian se usan para establecer los pines
+const int buttonPin = 2;     // el numero del pin del pulsador
+const int buzzerPin =  12;   // el numero de pin del zumbador
+
+// variables que cambian
+int buttonState = 0;       // variable para almacenar el estado del pulsador
+
+void setup() {
+  // inicializa el pulsador como entrada
+  pinMode(buttonPin, INPUT);
+  // inicializa el zumbador como salida
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  
+  // lee el estado del pulsador y lo almacena en la variable
+  buttonState = digitalRead(buttonPin);
+
+  // revisa si el pulsador esta presionado
+  if (buttonState == HIGH) {
+    digitalWrite(buzzerPin, HIGH);      // activa el zumbador
+  }
+  // si no esta presionado
+  else {
+    digitalWrite(buzzerPin, LOW);     // apaga el zumbador
+  }
+}
