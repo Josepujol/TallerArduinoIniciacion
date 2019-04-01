f63a9f084731f8552826d520b3dfaa0ce453897c
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2019-04-01 21:02:37 +0200

    Create PotenciometroLED.ino

---
+++ SalidasAnalogicas/PotenciometroLED/PotenciometroLED/PotenciometroLED.ino
@@ -0,0 +1,31 @@
+/*
+ Control de la lumninosidad de un LED con un potenciómetro
+ 
+ LED pin 9
+ Potenciometro pin A0
+ 
+ Noviembre 2017
+ Jose Pujol
+ */
+ 
+// Variables globales
+// Variables constantes para los pines
+const int potPin = A0; // selecciona el pin de entrada para el potenciómetro
+const int ledPin = 9;  // selecciona pin 9 para el LED 
+
+// variables que almacenan los datos
+int potValue = 0; // variable para almacenar los valores del potenciómetro 0-1023
+int bright = 0; // variable que almacena el valor del sensor mapeado 0-255
+
+void setup() {
+  // nada que declarar en el setup
+}
+
+void loop() {
+  // lee el valor del sensor 0-1023
+  potValue = analogRead(potPin);
+  // mapea el valor a escala 0-255
+  bright = map(potValue, 0, 1023, 0, 255);
+  // escribimos el valor en analogico en el LED
+  analogWrite(ledPin, bright);
+}
